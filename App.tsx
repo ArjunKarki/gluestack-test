6908bbaf949c95631f680b9b581ef5dc097c4d01
Aung Kyaw Soe <33275086+ArjunKarki@users.noreply.github.com>
2024-12-19 12:08:13 +0700

    test menu

--- App.tsx
+++ App.tsx
@@ -1,122 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import type {PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Pressable, Text, TouchableOpacity, View} from 'react-native';
 import {GluestackUIProvider} from './components/ui/gluestack-ui-provider';
+import {Menu, MenuItem, MenuItemLabel} from './components/ui/menu';
 import './global.css';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Menu
+        offset={5}
+        disabledKeys={['Settings']}
+        trigger={({...triggerProps}) => {
+          return (
+            <TouchableOpacity {...triggerProps}>
+              <Text>Home Menu</Text>
+            </TouchableOpacity>
+          );
+        }}>
+        <MenuItem key="Add account" textValue="Add account">
+          <MenuItemLabel size="sm">Add account</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Community" textValue="Community">
+          <MenuItemLabel size="sm">Community</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Plugins" textValue="Plugins">
+          <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Settings" textValue="Settings">
+          <MenuItemLabel size="sm">Settings</MenuItemLabel>
+        </MenuItem>
+      </Menu>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const Stack = createNativeStackNavigator();
+const App = () => {
   return (
-    <GluestackUIProvider mode="light">
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+    <GluestackUIProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{
+              headerRight: () => {
+                return (
+                  <Menu
+                    placement="left top"
+                    disabledKeys={['Settings']}
+                    trigger={({...triggerProps}) => {
+                      return (
+                        <Pressable {...triggerProps}>
+                          <Text>Menu</Text>
+                        </Pressable>
+                      );
+                    }}>
+                    <MenuItem key="Add account" textValue="Add account">
+                      <MenuItemLabel size="sm">Add account</MenuItemLabel>
+                    </MenuItem>
+                    <MenuItem key="Community" textValue="Community">
+                      <MenuItemLabel size="sm">Community</MenuItemLabel>
+                    </MenuItem>
+                    <MenuItem key="Plugins" textValue="Plugins">
+                      <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+                    </MenuItem>
+                    <MenuItem key="Settings" textValue="Settings">
+                      <MenuItemLabel size="sm">Settings</MenuItemLabel>
+                    </MenuItem>
+                  </Menu>
+                );
+              },
+            }}
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </GluestackUIProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
